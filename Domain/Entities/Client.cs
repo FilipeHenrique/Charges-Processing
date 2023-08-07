d5cf6ca86e57f1500c03de16e65a0c7e9a141ce7
Filipe <filipevp1999@hotmail.com>
2023-08-07 16:22:42 -0300

    chore: add base project setup

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class Client
+    {   
+        public Client(string name, string state, string cpf)
+        {
+            Name = name;
+            State = state;
+            CPF = cpf;
+        }
+
+        public string Name { get; private set; }
+        public string State { get; private set; }
+        public string CPF { get; private set; }
+
+    }
+}
