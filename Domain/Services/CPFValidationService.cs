4314e351e277f2c0f07ce43753240a9518cf1615
Filipe <filipevp1999@hotmail.com>
2023-08-09 23:06:08 -0300

    fix: cpf validation service names fixed

--- Domain/Services/CPFValidationService.cs
+++ Domain/Services/CPFValidationService.cs
@@ -4,39 +4,39 @@
     {
         public bool IsCpf(string cpf)
         {
-            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicator1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicator2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             string tempCpf;
-            string digito;
-            int soma;
-            int resto;
+            string digit;
+            int sum;
+            int mod;
 
             cpf = CPFToNumericString(cpf);
 
             if (cpf.Length != 11)
                 return false;
             tempCpf = cpf.Substring(0, 9);
-            soma = 0;
+            sum = 0;
 
             for (int i = 0; i < 9; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
-            resto = soma % 11;
-            if (resto < 2)
-                resto = 0;
+                sum += int.Parse(tempCpf[i].ToString()) * multiplicator1[i];
+            mod = sum % 11;
+            if (mod < 2)
+                mod = 0;
             else
-                resto = 11 - resto;
-            digito = resto.ToString();
-            tempCpf = tempCpf + digito;
-            soma = 0;
+                mod = 11 - mod;
+            digit = mod.ToString();
+            tempCpf = tempCpf + digit;
+            sum = 0;
             for (int i = 0; i < 10; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
-            resto = soma % 11;
-            if (resto < 2)
-                resto = 0;
+                sum += int.Parse(tempCpf[i].ToString()) * multiplicator2[i];
+            mod = sum % 11;
+            if (mod < 2)
+                mod = 0;
             else
-                resto = 11 - resto;
-            digito = digito + resto.ToString();
-            return cpf.EndsWith(digito);
+                mod = 11 - mod;
+            digit = digit + mod.ToString();
+            return cpf.EndsWith(digit);
         }
         public string CPFToNumericString(string cpf)
         {
