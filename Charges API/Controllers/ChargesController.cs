881b506d3f0cc6cdeabc5b84bf61e876bcf92441
Filipe <filipevp1999@hotmail.com>
2023-08-22 16:57:59 -0300

    fix: controllers unified

---
+++ Charges API/Controllers/ChargesController.cs
@@ -0,0 +1,69 @@
+﻿using Charges_API.DTO;
+using Domain.Contracts.UseCases.Charges;
+using Domain.Entities;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charges_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChargesController : Controller
+    {
+        private readonly IListChargesUseCase getChargesUseCase;
+        private readonly ICPFValidationService cpfValidationService;
+        private readonly ICreateChargeUseCase createChargeUseCase;
+        public ChargesController(IListChargesUseCase getChargesUseCase, ICPFValidationService cpfValidationService, ICreateChargeUseCase createChargeUseCase)
+        {
+            this.getChargesUseCase = getChargesUseCase;
+            this.cpfValidationService = cpfValidationService;
+            this.createChargeUseCase = createChargeUseCase;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCharge(CreateChargeDTO createChargeDTO)
+        {
+            if (!cpfValidationService.IsCpf(createChargeDTO.ClientCPF))
+            {
+                return BadRequest("Invalid CPF.");
+            }
+            string formattedCPF = cpfValidationService.CPFToNumericString(createChargeDTO.ClientCPF);
+            Charge newCharge = new Charge(createChargeDTO.Value, createChargeDTO.DueDate, formattedCPF);
+            createChargeUseCase.CreateCharge(newCharge);
+            return Created("", newCharge);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCharges(string? cpf = null, int? month = null)
+        {
+            if (string.IsNullOrWhiteSpace(cpf) && month == null)
+            {
+                return BadRequest("Atleast one query param is necessary, please specify cpf or dueDate");
+            }
+
+            if (!string.IsNullOrWhiteSpace(cpf) && month != null)
+            {
+                return BadRequest("Only one filter is permitted, choose between cpf or dueDate");
+            }
+
+            List<Charge>? charges = null;
+
+            if (!string.IsNullOrWhiteSpace(cpf))
+            {
+                if (!cpfValidationService.IsCpf(cpf))
+                {
+                    return BadRequest("Invalid CPF.");
+                }
+                string formattedCPF = cpfValidationService.CPFToNumericString(cpf);
+                charges = await getChargesUseCase.GetChargesByCPF(formattedCPF);
+            }
+
+            if (month != null)
+            {
+                charges = await getChargesUseCase.GetChargesByMonth((int)month);
+            }
+
+            return Ok(charges);
+        }
+    }
+}
---
+++ Clients API/Controllers/ClientsController.cs
@@ -0,0 +1,79 @@
+﻿using Clients_API.DTO;
+using Clients_API.Mappers;
+using Domain.Contracts.UseCases.Clients;
+using Domain.Entities;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clients_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClientsController : Controller
+    {
+        private readonly ICreateClientUseCase createClientUseCase;
+
+        private readonly IGetClientUseCase getClientUseCase;
+
+        private readonly IListClientsUseCase listClientsUseCase;
+
+        private readonly ICPFValidationService cpfValidationService;
+
+        public ClientsController(ICreateClientUseCase createClientUseCase, ICPFValidationService cPFValidationService, IGetClientUseCase getClientUseCase, IListClientsUseCase listClientsUseCase)
+        {
+            this.createClientUseCase = createClientUseCase;
+            this.getClientUseCase = getClientUseCase;
+            this.listClientsUseCase = listClientsUseCase;
+            this.cpfValidationService = cPFValidationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateClient(CreateClientDTO createClientDTO)
+        {
+            if (!cpfValidationService.IsCpf(createClientDTO.CPF))
+            {
+                return BadRequest("Invalid CPF.");
+            }
+
+            string formattedCPF = cpfValidationService.CPFToNumericString(createClientDTO.CPF);
+            createClientDTO.CPF = formattedCPF;
+
+            Client client = await getClientUseCase.GetClient(createClientDTO.CPF);
+
+            if (client != null)
+            {
+                return BadRequest("CPF already exists.");
+            }
+
+            Client newClient = ClientMapper.ToClient(createClientDTO);
+            createClientUseCase.CreateClient(newClient);
+            return Created("", newClient);
+        }
+
+        [HttpGet("{cpf}")]
+        public async Task<IActionResult> GetClient(string cpf)
+        {
+            if (!cpfValidationService.IsCpf(cpf))
+            {
+                return BadRequest("Invalid CPF.");
+            }
+
+            string formattedCPF = cpfValidationService.CPFToNumericString(cpf);
+            Client client = await getClientUseCase.GetClient(formattedCPF);
+
+            if (client == null)
+            {
+                return NotFound("Client not found.");
+            }
+
+            return Ok(client);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> ListClients()
+        {
+            List<Client> clients = await listClientsUseCase.ListClients();
+            return Ok(clients);
+        }
+    }
+}
