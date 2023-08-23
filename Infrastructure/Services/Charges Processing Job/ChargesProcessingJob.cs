fb53a5baed491b2715778df322bd092d64a1593e
Filipe <filipevp1999@hotmail.com>
2023-08-23 19:48:29 -0300

    fix: divides responsabilities in charges processing job

--- Infrastructure/Services/Charges Processing Job/ChargesProcessingJob.cs
+++ Infrastructure/Services/Charges Processing Job/ChargesProcessingJob.cs
@@ -19,51 +19,63 @@ namespace Charges_Processing_Job
         {
             try
             {
-                HttpResponseMessage clientsResponse = await httpClient.GetAsync("http://localhost:7085/clients");
-
-                List<(string state, float value)> chargesByState = new List<(string, float)>();
-
-                if (clientsResponse.IsSuccessStatusCode)
-                {
-                    string responseBody = await clientsResponse.Content.ReadAsStringAsync();
-                    IAsyncEnumerable<Client> clients = JsonSerializer.Deserialize<IAsyncEnumerable<Client>>(responseBody);
-
-                    await foreach (Client client in clients)
-                    {
-                        string extractedDigits = $"{client.CPF[0]}{client.CPF[1]}{client.CPF[9]}{client.CPF[10]}";
-                        float chargeValue = float.Parse(extractedDigits);
-
-                        DateTime currentDate = DateTime.Now;
-                        DateTime oneMonthFromNow = currentDate.AddMonths(1);
-
-                        Charge charge = new Charge(chargeValue, oneMonthFromNow, client.CPF);
-
-                        var content = new StringContent(JsonSerializer.Serialize(charge), System.Text.Encoding.UTF8, "application/json");
-
-                        HttpResponseMessage chargeResponse = await httpClient.PostAsync("http://localhost:7289/charges", content);
-                       
-                        chargesByState.Add((client.State, chargeValue));
-                    }
-
-                    // MapReduce
-                    var totalChargesByState = chargesByState
-                        .GroupBy(charge => charge.state)
-                        .Select(group => new
-                        {
-                            State = group.Key,
-                            Total = group.Sum(charge => charge.value)
-                        })
-                        .OrderBy(charge => charge.State);
-
-                    foreach (var charge in totalChargesByState) { 
-                        _logger.LogInformation($"{charge.State}: {charge.Total}"); 
-                    }
-                }
+                var response = await GetClients();
+                var clients = JsonSerializer.Deserialize<IAsyncEnumerable<Client>>(response);
+                var chargesByState = await ProcessCharges(clients);
+                MapReduce(chargesByState);            
             }
             catch (Exception ex)
             {
                 _logger.LogInformation($"ERROR: {ex.Message}");
-                throw new HttpRequestException($"Request failed with status code: {ex.Message}");
+                throw new HttpRequestException($"Processing failed with error: {ex.Message}");
+            }
+        }
+
+        private async Task<string> GetClients()
+        {
+            var clientsResponse = await httpClient.GetAsync("http://localhost:7085/clients");
+            clientsResponse.EnsureSuccessStatusCode();
+            return await clientsResponse.Content.ReadAsStringAsync();
+        }
+
+        private async Task<List<(string state, float value)>> ProcessCharges(IAsyncEnumerable<Client> clients)
+        {
+            List<(string state, float value)> chargesList = new List<(string, float)>();
+
+            await foreach (Client client in clients)
+            {
+                var extractedDigits = $"{client.CPF[0]}{client.CPF[1]}{client.CPF[9]}{client.CPF[10]}";
+                var chargeValue = float.Parse(extractedDigits);
+
+                var currentDate = DateTime.Now;
+                var oneMonthFromNow = currentDate.AddMonths(1);
+
+                var charge = new Charge(chargeValue, oneMonthFromNow, client.CPF);
+
+                var content = new StringContent(JsonSerializer.Serialize(charge), System.Text.Encoding.UTF8, "application/json");
+                var chargeResponse = await httpClient.PostAsync("http://localhost:7289/charges", content);
+                chargeResponse.EnsureSuccessStatusCode();
+
+                chargesList.Add((client.State, chargeValue));
+            }
+
+            return chargesList;
+        }
+
+        private void MapReduce(List<(string state, float value)> chargesList)
+        {
+            var totalChargesByState = chargesList
+                .GroupBy(charge => charge.state)
+                .Select(group => new
+                {
+                    State = group.Key,
+                    Total = group.Sum(charge => charge.value)
+                })
+                .OrderBy(charge => charge.State);
+
+            foreach (var charge in totalChargesByState)
+            {
+                _logger.LogInformation($"{charge.State}: {charge.Total}");
             }
         }
     }
