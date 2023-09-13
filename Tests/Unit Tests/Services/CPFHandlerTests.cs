3a51fb459dba737d0d531c7767ba8b483e01658c
Filipe <filipevp1999@hotmail.com>
2023-09-13 17:52:16 -0300

    test: cpf handler unit tests

---
+++ Tests/Unit Tests/Services/CPFHandlerTests.cs
@@ -0,0 +1,55 @@
+﻿using Infrastructure.Services;
+
+namespace Tests.Unit_Tests.Services
+{
+
+    public class CPFHandlerTests
+    {
+        private CPFHandler cpfHandler;
+
+        public CPFHandlerTests()
+        {
+            cpfHandler = new CPFHandler();
+        }
+
+        [Fact]
+        public void IsCpf_ValidCpf_ReturnsTrue()
+        {
+            // Arrange
+            string validCpf = "463.264.620-29";
+
+            // Act
+            bool result = cpfHandler.IsCpf(validCpf);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsCpf_InvalidCpf_ReturnsFalse()
+        {
+            // Arrange
+            string invalidCpf = "463.264.620-21";
+
+            // Act
+            bool result = cpfHandler.IsCpf(invalidCpf);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CPFToNumericString_ValidCpf_ReturnsNumericString()
+        {
+            // Arrange
+            string cpf = "463.264.620-29";
+            string expectedFormattedCPF = "46326462029";
+
+            // Act
+            string result = cpfHandler.CPFToNumericString(cpf);
+
+            // Assert
+            Assert.Equal(expectedFormattedCPF, result);
+        }
+    }
+}
